792de12b1f1f322179ff1560751f6a737d74d2f7
BulbaWarrior <42455524+BulbaWarrior@users.noreply.github.com>
2020-12-27 00:12:19 +0300

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ rm /tmp/image.tar
 ssh $OPT $SITE sudo docker load -i /tmp/image.tar
 ssh $OPT $SITE sudo docker stop django-redis-chat
 ssh $OPT $SITE sudo docker rm django-redis-chat
-ssh $OPT $SITE sudo docker run -p 80:8000 -v webapp:/web --name django-redis-chat -d --env POSTGRES=$DATABASE_ADDR --env REDIS=$DATABASE_ADDR --env NODE_ID=$NODE_ID django-redis-chat:$BUILD_ID bash -c "python3 manage.py makemigrations && python3 manage.py migrate --run-syncdb && python3 -u manage.py runserver 0.0.0.0:8000 --noreload"
+ssh $OPT $SITE sudo docker run -p 80:8000 -v webapp:/web --name django-redis-chat -d --env POSTGRES=$DATABASE_ADDR --env REDIS=$DATABASE_ADDR --env NODE_ID=$NODE_ID redis-chat:$BUILD_ID bash -c "python3 manage.py makemigrations && python3 manage.py migrate --run-syncdb && python3 -u manage.py runserver 0.0.0.0:8000 --noreload"
 
 ssh $OPT $SITE rm /tmp/image.tar
 echo deployed
