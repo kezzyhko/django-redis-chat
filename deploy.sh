ec45a1e229719c3e2147411e560a3f5e70d82d25
BulbaWarrior <vladkalm49@gmail.com>
2020-12-29 15:09:25 +0300

    move remotely executed part of the script to a separate file

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,8 @@ do
     docker save -o /tmp/image.tar redis-chat:$BUILD_ID
     scp $OPT /tmp/image.tar $SITE:/tmp/image.tar
     rm /tmp/image.tar
-    ssh $OPT $SITE sudo docker load -i /tmp/image.tar
-    ssh $OPT $SITE sudo docker stop django-redis-chat
-    ssh $OPT $SITE sudo docker rm django-redis-chat
-    ssh $OPT $SITE sudo docker run -p 80:8000 -v webapp:/web --name django-redis-chat -d --env POSTGRES=$DATABASE_ADDR --env REDIS=$DATABASE_ADDR --env NODE_ID=$NODE_ID redis-chat:$BUILD_ID bash -c "python3 manage.py makemigrations && python3 manage.py migrate --run-syncdb && python3 -u manage.py runserver 0.0.0.0:8000 --noreload"
-
-    ssh $OPT $SITE rm /tmp/image.tar
-
+    cat web_remote.sh | ssh $OPT $SITE
+    
 done
 echo deployed
 
