308de549e58b147698517da48e23c56e47d67675
BulbaWarrior <42455524+BulbaWarrior@users.noreply.github.com>
2021-01-17 17:20:25 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@ do
     OPT="-i ${SSH_CRED} -o StrictHostKeyChecking=no"
     SITE="${SSH_CRED_USR}@${SERVER_IP}"
 
-    envsubst < web_remote.sh | ssh $OPT $SITE
+    ssh $OPT $SITE sudo docker stop django-redis-chat && sudo docker rm django-redis-chat
+    ssh $OPT $SITE sudo docker build https://github.com/kezzyhko/django-redis-chat.git#$BRANCH_NAME --tag django-redis-chat
+    ssh $OPT $SITE sudo docker run -p 80:8000 -v webapp:/web --name django-redis-chat -d --env POSTGRES=$DATABASE_ADDR --env REDIS=$DATABASE_ADDR --env NODE_ID=$NODE_ID redis-chat:$BUILD_ID bash -c "python3 manage.py makemigrations && python3 manage.py migrate --run-syncdb && python3 -u manage.py runserver 0.0.0.0:8000 --noreload"
     
 done
 echo deployed $BRANCH_NAME
