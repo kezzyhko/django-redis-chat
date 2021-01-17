130c16f2d53cc36d0994a57b119603d8876bc6f8
BulbaWarrior <42455524+BulbaWarrior@users.noreply.github.com>
2021-01-17 16:58:50 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ do
     OPT="-i ${SSH_CRED} -o StrictHostKeyChecking=no"
     SITE="${SSH_CRED_USR}@${SERVER_IP}"
 
-    envsubst \${BRANCH_NAME} \${DATABASE_ADDR} \${NODE_ID} \${BUILD_ID} < web_remote.sh | ssh $OPT $SITE
+    envsubst < web_remote.sh | ssh $OPT $SITE
     
 done
 echo deployed $BRANCH_NAME
